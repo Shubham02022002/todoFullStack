c5932837a11b2b3eba34e7045163dc7cfd62ddcc
Shubham02022002 <chaudhary2001shubham@gmail.com>
2025-12-28 13:23:38 +0530

    + useRef hook

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useRef } from "react";
+
 const LoginForm = ({ formData, setFormData, onSignin, onSignup, loading }) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
     <fieldset className="fieldset bg-base-200 border rounded-box w-xs p-4 m-10">
       <legend className="fieldset-legend">Login</legend>
 
       <label>Email</label>
       <input
+        ref={inputRef}
         value={formData.username}
         onChange={(e) =>
           setFormData((p) => ({ ...p, username: e.target.value }))
